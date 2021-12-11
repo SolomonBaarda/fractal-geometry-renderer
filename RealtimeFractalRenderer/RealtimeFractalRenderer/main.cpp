245a0f10d78ae2cb1b66f879db4247d785102c64
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2021-12-11 16:34:26 +0000

    Added basic timer

--- RealtimeFractalRenderer/RealtimeFractalRenderer/main.cpp
+++ RealtimeFractalRenderer/RealtimeFractalRenderer/main.cpp
@@ -1,23 +1,37 @@
-﻿#include <cstdint>
-#include "Controller.h"
+﻿#include "Controller.h"
 #include "Renderer.h"
 #include "Display.h"
 
-
+#include <cstdint>
+#include <string>
+#include <windows.h> // required for profileapi
+#include <profileapi.h>
 
 int main()
 {
+	LARGE_INTEGER frequency, t1, t2;
+	// Get ticks per second
+	QueryPerformanceFrequency(&frequency);
+
 	const uint32_t width = 900, height = 600;
 
 	Controller c;
 	Display d(width, height);
 	Renderer r(width, height);
 
+
+	QueryPerformanceCounter(&t1); // START TIMER
+
 	r.render();
 	//d.saveToFile(r.buffer, width, height, "file.ppm");
 	d.set_pixels(r.buffer);
 
-	std::cout << "rendered";
+	QueryPerformanceCounter(&t2); // END TIMER
+
+	// Compute elapsed time in milliseconds
+	double elapsed_time_ms = (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
+
+	std::cout << "Rendered and displayed frame in " << std::to_string(elapsed_time_ms) << " milliseconds\n";
 
 	while (true)
 	{
