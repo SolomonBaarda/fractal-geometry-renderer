79da5c93aa9e361ef4c7c9d9d2f30733204b3d65
Solomon Baarda <solomon_baarda@outlook.com>
2022-02-18 11:26:47 +0000

    Fixed unit tests

--- FractalGeometryRenderer/FractalGeometryRenderer/test/unittest/test2.cpp
+++ FractalGeometryRenderer/FractalGeometryRenderer/test/unittest/test2.cpp
@@ -1,10 +1,5 @@
-#define BOOST_TEST_MODULE MyTest2
+#include <gtest/gtest.h>
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(test_1) 
-{
-    BOOST_CHECK_EQUAL(1, 1);
-
-    printf("TEST FILE 1");
+TEST(ABC, TEST1) {
+	EXPECT_EQ(true, true);
 }
