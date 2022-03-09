1249edcea4517b4030b014878f9361d7a3b6c0da
Solomon Baarda <solomon_baarda@outlook.com>
2022-03-09 17:16:31 +0000

    Ran mandelbulb benchmark again

--- FractalGeometryRenderer/FractalGeometryRenderer/runBenchmarks.bat
+++ FractalGeometryRenderer/FractalGeometryRenderer/runBenchmarks.bat
@@ -1,50 +1,26 @@
+@echo off
+SETLOCAL
 
 :: Mandelbulb optimisations
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/mandelbulb_optimisations/no_optimisations.cl" 
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/mandelbulb_optimisations/linear_epsilon.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/mandelbulb_optimisations/bounding_volume.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_optimisations/no_optimisations.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_optimisations/all_optimisations.cl"
-timeout /t 45 /nobreak
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/optimisations_none.cl -f"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/optimisations_intersection_epsilon.cl -f"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/optimisations_bounding_volume.cl -f"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/optimisations_none.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/optimisations_all.cl"
 
 :: Mandelbulb features
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_features/no_features.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_features/hard_shadows.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_features/phong.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/mandelbulb_features/all_features.cl"
-timeout /t 45 /nobreak
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_none.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_phong.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_glow.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_hard_shadows.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_soft_shadows.cl"
+CALL :Benchmark "-r 1920 1080 -s kernels/benchmarks/mandelbulb/features_all.cl"
 
+pause
 
+:: Function for running multiple benchmarks for one configuration
+:Benchmark
 
-goto skip
+for /l %%x in (1, 1, 1) do FractalGeometryRenderer.exe %~1
 
-:: Sierpinski optimisations
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/sierpinski_optimisations/no_optimisations.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/sierpinski_optimisations/linear_epsilon.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -f -s "kernels/benchmarks/sierpinski_optimisations/bounding_volume.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_optimisations/no_optimisations.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_optimisations/all_optimisations.cl"
-timeout /t 45 /nobreak
-
-:: Sierpinski features
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_features/no_features.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_features/hard_shadows.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_features/phong.cl"
-timeout /t 45 /nobreak
-FractalGeometryRenderer.exe -r 1920 1080 -s "kernels/benchmarks/sierpinski_features/all_features.cl"
-timeout /t 45 /nobreak
-
-:skip
+EXIT /B 0
